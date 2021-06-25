9c7f6b499cee826e2fc96922e466357b7a307dda
aarish21 <aarish.jerry@gmail.com>
2021-06-26 00:28:54 +0530

    Initial Commit

---
+++ Word Scramble/Word_ScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Word_ScrambleApp.swift
+//  Word Scramble
+//
+//  Created by Aarish Rahman on 26/06/21.
+//
+
+import SwiftUI
+
+@main
+struct Word_ScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
